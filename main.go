7a414c3a382e376be429957a32c74c899105b742
DavidEsdrs <carpina1983@gmail.com>
2024-07-28 21:00:45 -0300

    feat: install gin and start server

--- main.go
+++ main.go
@@ -5,15 +5,16 @@ import (
 	"log"
 
 	"github.com/DavidEsdrs/go-mercado/models"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func main() {
+func setupDatabase() error {
 	dsn := "root:mysqlPW@tcp(db:3306)/db?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	err = db.AutoMigrate(
@@ -24,8 +25,25 @@ func main() {
 	)
 
 	if err != nil {
-		log.Fatalf("Failed to auto-migrate: %v", err)
+		return err
 	}
 
 	fmt.Println("Auto-migration completed successfully.")
+	return nil
+}
+
+func main() {
+	fmt.Println("starting...")
+	err := setupDatabase()
+	if err != nil {
+		log.Fatal("error while starting database: ", err.Error())
+	}
+
+	r := gin.Default()
+
+	r.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(200, "{success: true}")
+	})
+
+	r.Run(":8080")
 }
