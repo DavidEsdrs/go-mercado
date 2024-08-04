d5723829232d82c93d5f9b7921e782e53e11ef3f
DavidEsdrs <carpina1983@gmail.com>
2024-08-04 15:17:59 -0300

    feat: Password field is 100 characters long

--- internal/model/user.go
+++ internal/model/user.go
@@ -8,5 +8,5 @@ type User struct {
 	gorm.Model
 	Username     string `gorm:"type:varchar(100);not null" json:"username"`
 	Email        string `gorm:"type:varchar(100);not null;unique" json:"email"`
-	HashPassword string `gorm:"type:varchar(50);not null" json:"hash_password"`
+	HashPassword string `gorm:"type:varchar(100);not null" json:"hash_password"`
 }
